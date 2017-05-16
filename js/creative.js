485616b794626ad71b4eb40a5dcdfd23858d7a50
reoliveira <oliveira.reid@gmail.com>
2017-05-15 22:41:03 -0700

    removing scroll reveal

--- js/creative.js
+++ js/creative.js
@@ -28,23 +28,6 @@
         }
     })
 
-    // Initialize and Configure Scroll Reveal Animation
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
-
     // Initialize and Configure Magnific Popup Lightbox Plugin
     $('.popup-modal').magnificPopup({
 		type: 'inline',
